bb9aa3fc767f20dc710dfd29a2c783f90cde9543
Cosmin Mihu <cosmin.mihu@gmail.com>
2022-08-03 21:01:09 +0300

    App Link

---
+++ app/src/main/java/ro/mdc_software/androidlinks/app/AppLinkActivity.kt
@@ -0,0 +1,28 @@
+package ro.mdc_software.androidlinks.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+
+private const val TAG = "AppLinkActivity"
+
+class AppLinkActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val deepLink = intent.data
+        val deviceId = deepLink?.getQueryParameter("deviceId")
+        val appId = deepLink?.getQueryParameter("appId")
+
+        setContent {
+            Column {
+                Text(TAG)
+                Text("deviceId = $deviceId")
+                Text("appId = $appId")
+            }
+        }
+    }
+}
